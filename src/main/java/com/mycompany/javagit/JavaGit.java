fc8ac49be7c9c718ace64a02f5ccc8321bb40e9f
KiraLujza <kira.lujza@gmail.com>
2024-10-24 16:02:43 +0200

    Refactorizalas

--- src/main/java/com/mycompany/javagit/JavaGit.java
+++ src/main/java/com/mycompany/javagit/JavaGit.java
@@ -4,6 +4,14 @@ package com.mycompany.javagit;
 public class JavaGit {
 
     public static void main(String[] args) {
+        final int HIDEG_HATAR = 10;
+       int homerseklet = 7;
+        boolean hidegVan = homerseklet < HIDEG_HATAR;
+        String eredmeny = "viszek kabátot";
+        eredmeny = hidegVan ? eredmeny : "nem" + eredmeny;
+
+        System.out.println(eredmeny);
         
     }
 }
+    
